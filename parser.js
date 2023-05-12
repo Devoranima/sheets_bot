57c2b7b6431064ec6cbbb03c6ae8232c661f4e93
Sepia <ginzzu_doc@mail.com>
2023-05-12 11:57:46 +0300

    init

---
+++ parser.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const wallets = fs.readFileSync('./wallets.txt').toString().split(/[\r,\n]+/).filter(e => e != '');
+const address = '0x2f90907fD1DC1B7a484b6f31Ddf012328c2baB28';
+const len = wallets.length;
+
+(async function (){
+    clear('./result.txt')
+    for (let i = 0; i < len; i++){
+        const wallet = wallets[i]
+        await parseData(wallet, i==(len-1));
+    }
+    
+})()
+
+async function parseData(wallet, isLast){
+    const response = (await axios.get('https://explorer.dogechain.dog/api', {
+        params: {
+            module:'account',
+            action:'tokenbalance',
+            contractaddress:address,
+            address:wallet
+        }
+    }))
+    if (response.data.result){
+        const value = (Math.round(response.data.result/(10**18)*100)/100)
+        write('./result.txt', value + (isLast == 0 ? '\n' : ''));
+    }
+    else console.log('network too busy...');
+}
+
+
+//Write to file
+function write(path_to_file, content){
+    fs.writeFile(path_to_file, content, { flag: 'a' }, err => {
+        if (err) {
+          console.error(err);
+        }
+    });
+}
+
+function clear(filename){
+    fs.writeFile(filename, "", err =>{
+        if (err) {
+            console.error(err);
+        }
+    });
+}
