9daa486c61c8fc30e5ab9dc85ce49e8c36027cc0
Devoranima <ali3v5t1limAw@gmail.com>
2023-05-13 20:03:30 +0300

    made stable version with all features

--- sentinel_v1.js
+++ sentinel_v1.js
@@ -21,25 +21,9 @@ const N = 1000;
     // const morrons = doc.sheetsByIndex[1];
     let day_start = doc.sheetsByTitle.DayStart;
     await day_start.loadCells('A2:B3');
-    
-//    /////////////////////////////////////////////////////////////////////////////// 
-//    await day_start.updateProperties({ title: getCurrentDate() });
 
-//    const new_sheet = await doc.addSheet({title:'DayStart'});
 
-//    await new_sheet.setHeaderRow(['address', 'start'], 1);
-
-//    await new_sheet.loadCells('A2:B3');
-//    for (let i = 0; i < 2; i++){
-//        for (let j = 0; j < 2;j++){
-//            new_sheet.getCell(i+1, j).value = day_start.getCell(i+1, j).value
-//        }
-//    }
-//    await new_sheet.saveUpdatedCells();
-
-//    day_start = new_sheet;
 //////////////////////////////////////////////////////////////////////////////////
-//    await dayStartCorrection(day_start);
     
 //    let new_day = setInterval( async function(){ 
 //        const date = new Date();
@@ -71,6 +55,18 @@ const N = 1000;
 //        }
 //    }, 1000*60*60);
 
+    let new_day = setInterval( async function(){ 
+        const date = new Date();
+        let hour = date.getHours();
+        if (hour == 1) {
+            try {
+                await dayStartCorrection(day_start);
+            } catch (error) {
+                write('./logs.txt', 'Error at Day Start: ' + error.toString()+'\n');
+            }
+        }
+    }, 1000*60*60);
+
     let scanner = setInterval(() => {
         try {
             monitor(top, day_start);
@@ -82,7 +78,7 @@ const N = 1000;
 
 async function monitor(top, day_start){
     
-    const response = (await axios.get('https://explorer.dogechain.dog/api', {
+    await axios.get('https://explorer.dogechain.dog/api', {
         params: {
             module:'token',
             action:'getTokenHolders',
@@ -90,73 +86,102 @@ async function monitor(top, day_start){
             page:0,
             offset:N
         }
-    }))
-    if (response.data.result){
-        //* loads range of cells into local cache - DOES NOT RETURN THE CELLS
-        await top.loadCells('A2:D1000');
+    }).then(async (response)=>{
+        if (response.data.result){
+            //* loads range of cells into local cache - DOES NOT RETURN THE CELLS
+            await top.loadCells('A2:D1000');
+        
+        ////////////////////////////////////////////////////////////////
     
-    ////////////////////////////////////////////////////////////////
-
-        //*Wallets data we get from our Dogechain Explorer
-        let api_current_wallets = response.data.result;
-        api_current_wallets = [...api_current_wallets.map(e=>{
-            return {
-                address: e.address,
-                current: (Math.round(e.value/(10**18)*100)/100)
+            //*Wallets data we get from our Dogechain Explorer
+            let api_current_wallets = response.data.result;
+            api_current_wallets = [...api_current_wallets.map(e=>{
+                return {
+                    address: e.address,
+                    current: (Math.round(e.value/(10**18)*100)/100)
+                }
+            })]
+    
+            //*Array of current top N addresses 
+            const api_current_addresses = [...api_current_wallets.map(e => e.address)];
+    
+        ////////////////////////////////////////////////////////////////
+            
+            try {
+                await correctTop(top, day_start, api_current_wallets, api_current_addresses);
+            } catch (error) {
+                write('./logs.txt', 'Error at Top Sheet Correction function: ' + error.toString()+'\n');
             }
-        })]
-
-        //*Array of current top N addresses 
-        const api_current_addresses = [...api_current_wallets.map(e => e.address)];
-
-    ////////////////////////////////////////////////////////////////
-
-        //*Raw rows from Google Sheets
-        const sheet_current_rows = await top.getRows();
-        
-        //*Wallets data we get from our Google Sheets
-        let sheet_current_wallets = [...sheet_current_rows.map(e =>{
-            return {
-                address: e.address,
-                start: e.start,
-                current: e.current
-            }
-        })];
-        sheet_current_wallets = [...sheet_current_wallets.filter((e, index)=> (index > 2 && e.address != ''))]
-
-        //*Array of sheets top N addresses
-        const sheet_current_addresses = [...sheet_current_wallets.map(e=>e.address)];
-
-    ///////////////////////////////////////////////////////////////
-
-        //*Raw rows from Day Start Sheet
-        const sheet_day_start_rows = await day_start.getRows();
-        
-        //*Wallets data we get from our Day Start Sheet
-        let sheet_day_start_wallets = [...sheet_day_start_rows.map(e =>{
-            return {
-                address: e.address,
-                start: e.start
-            }
-        })];
-        sheet_day_start_wallets = [...sheet_day_start_wallets.filter((e, index)=> index > 1)]
-
-        //*Array of Day Start Sheet's addresses
-        const sheet_day_start_addresses = [...sheet_day_start_wallets.map(e=>e.address)];
-
-        // try {
-        //     await correctAlert(alert, api_current_addresses, sheet_current_wallets);
-        // } catch (error) {
-        //     write('./logs.txt', 'Error at Alert Sheet Correction function: ' + error.toString()+'\n');
-        // }
-        
-        try {
-            await correctTop(top, day_start, api_current_wallets, api_current_addresses, sheet_day_start_wallets, sheet_day_start_addresses);
-        } catch (error) {
-            write('./logs.txt', 'Error at Top Sheet Correction function: ' + error.toString()+'\n');
         }
-    }
-    else console.log('network too busy...');
+    }).catch((e)=>{
+        console.log('network too busy...')
+    })
+    //if (response.data.result){
+    //    //* loads range of cells into local cache - DOES NOT RETURN THE CELLS
+    //    await top.loadCells('A2:D1000');
+    
+    //////////////////////////////////////////////////////////////////
+
+    //    //*Wallets data we get from our Dogechain Explorer
+    //    let api_current_wallets = response.data.result;
+    //    api_current_wallets = [...api_current_wallets.map(e=>{
+    //        return {
+    //            address: e.address,
+    //            current: (Math.round(e.value/(10**18)*100)/100)
+    //        }
+    //    })]
+
+    //    //*Array of current top N addresses 
+    //    const api_current_addresses = [...api_current_wallets.map(e => e.address)];
+
+    //////////////////////////////////////////////////////////////////
+
+    //    ////*Raw rows from Google Sheets
+    //    //const sheet_current_rows = await top.getRows();
+        
+    //    ////*Wallets data we get from our Google Sheets
+    //    //let sheet_current_wallets = [...sheet_current_rows.map(e =>{
+    //    //    return {
+    //    //        address: e.address,
+    //    //        start: e.start,
+    //    //        current: e.current
+    //    //    }
+    //    //})];
+    //    //sheet_current_wallets = [...sheet_current_wallets.filter((e, index)=> (index > 2 && e.address != ''))]
+
+    //    ////*Array of sheets top N addresses
+    //    //const sheet_current_addresses = [...sheet_current_wallets.map(e=>e.address)];
+
+    /////////////////////////////////////////////////////////////////
+
+    //    ////*Raw rows from Day Start Sheet
+    //    //const sheet_day_start_rows = await day_start.getRows();
+        
+    //    ////*Wallets data we get from our Day Start Sheet
+    //    //let sheet_day_start_wallets = [...sheet_day_start_rows.map(e =>{
+    //    //    return {
+    //    //        address: e.address,
+    //    //        start: e.start
+    //    //    }
+    //    //})];
+    //    //sheet_day_start_wallets = [...sheet_day_start_wallets.filter((e, index)=> index > 1)]
+
+    //    ////*Array of Day Start Sheet's addresses
+    //    //const sheet_day_start_addresses = [...sheet_day_start_wallets.map(e=>e.address)];
+
+    //    // try {
+    //    //     await correctAlert(alert, api_current_addresses, sheet_current_wallets);
+    //    // } catch (error) {
+    //    //     write('./logs.txt', 'Error at Alert Sheet Correction function: ' + error.toString()+'\n');
+    //    // }
+        
+    //    try {
+    //        await correctTop(top, day_start, api_current_wallets, api_current_addresses);
+    //    } catch (error) {
+    //        write('./logs.txt', 'Error at Top Sheet Correction function: ' + error.toString()+'\n');
+    //    }
+    //}
+    //else console.log('network too busy...');
 }
 
 async function correctAlert(ex, api_current_addresses, sheet_current_wallets){
@@ -183,32 +208,32 @@ async function correctAlert(ex, api_current_addresses, sheet_current_wallets){
     await ex.addRows([...array_alerts])
 }
 
-async function correctTop(top, day_start, api_current_wallets, api_current_addresses, sheet_day_start_wallets, sheet_day_start_addresses){
-    const newcumers = [];
+async function correctTop(top, day_start, api_current_wallets, api_current_addresses){
+    //const newcumers = [];
     for (let i = 0; i < api_current_addresses.length; i++){
         const api_address = api_current_addresses[i];
         const api_current_holds = api_current_wallets[i].current; 
 
-        const cell_address = top.getCell(i+3, 0);
-        const cell_start = top.getCell(i+3, 1);
-        const cell_current = top.getCell(i+3, 2);
+        const cell_address = top.getCell(i+3, 1);
+        //const cell_start = top.getCell(i+3, 2);
+        const cell_current = top.getCell(i+3, 3);
 
         
-        const index_in_start = sheet_day_start_addresses.indexOf(api_address);
-        let start_hold;
-        if (index_in_start == -1) {
-            start_hold = 0;
-            newcumers.push({
-                address: api_address,
-                start: 0
-            })
-        }
-        else start_hold = sheet_day_start_wallets[index_in_start].start;
+        //const index_in_start = sheet_day_start_addresses.indexOf(api_address);
+        //let start_hold;
+        //if (index_in_start == -1) {
+        //    start_hold = 0;
+        //    newcumers.push({
+        //        address: api_address,
+        //        start: 0
+        //    })
+        //}
+        //else start_hold = sheet_day_start_wallets[index_in_start].start;
         cell_address.value = api_address;
-        cell_start.value = start_hold;
+        //cell_start.value = start_hold;
         cell_current.value = api_current_holds;
     }
-    await day_start.addRows([...newcumers]);
+    //await day_start.addRows([...newcumers]);
     await top.saveUpdatedCells();
 }
 
